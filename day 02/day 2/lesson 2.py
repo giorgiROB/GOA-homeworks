9a73cb9e818c9db655456dfd59092faae7e0689f
giorgiROB <iogio924@gmail.com>
2024-05-07 15:05:03 +0400

    commit

--- day 02/day 2/lesson 2.py
+++ day 02/day 2/lesson 2.py
@@ -1,4 +1,4 @@
 
 name = "I met and befriended three guys. Their names are Nika Dato and Sandro. Sandro is also my mentor "
-surname = "I at the Academy in Goa like everything about how kind and helpful they are especially mentors. My mentor is Sandro and he will help me with everything if I have any difficulties. And those who study with me are glad that they have interests like me and almost the same lifestyle for example exercise programming and everything is in order "
+surname = "I at the Academy in Goa like everything about how kind and helpful they are especially group liders. My lider is Sandro and he will help me with everything if I have any difficulties. And those who study with me are glad that they have interests like me and almost the same lifestyle for example exercise programming and everything is in order "
 print (name + surname)
--- day 4/day 4/lesson 4.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("please enter your number: "))
-
-num2 = int(input("please enter your number: "))
-
-print(num1 + num2)
